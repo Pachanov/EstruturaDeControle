19ed4db34c1eaba24859fda9360256f6575bb3ba
Victor Hugo Pachano Maurera <pachanovictor94@gmail.com>
2023-02-16 20:56:26 -0300

    Mais um commit

---
+++ EstruturaDeControle1/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a pontuaçao de um aluno A(0-1000):");
+        var EstudanteA = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a pontuaçao de um aluno B(0-1000):");
+        var EstudanteB = int.Parse(Console.ReadLine());
+
+        var NotaDiferente = EstudanteA - EstudanteB;
+
+        Console.WriteLine("Utilizando if-else");
+
+        if (NotaDiferente > 0){
+            Console.WriteLine("Aluno A tem maior nota.");
+        }else if (NotaDiferente == 0){
+            Console.WriteLine("Empate.");
+        }else{
+            Console.WriteLine("Aluno B tem maior nota.");
+        }
+
+        Console.WriteLine("Utilizando swuit-case ");
+
+        switch (NotaDiferente){
+            case > 0:
+            Console.WriteLine("Aluno A tem maior nota.");
+            break;
+
+            case 0:
+            Console.WriteLine("Empate.");
+            break;
+
+            default:
+            Console.WriteLine("Aluno B tem maior nota.");
+            break;
+
+        }
+
+    }
+}
